95a9a4a0083d2514f4ecae3ad3da53d04f830979
Haobin Guo <haobin.guo@estudiantat.upc.edu>
2022-09-23 17:52:30 +0200

    SolucionInicial v1.ganancias

--- src/Central/Estado.java
+++ src/Central/Estado.java
@@ -14,6 +14,8 @@ public class Estado {
     private int[] estado;
     private double[] productionNoConsumed;
 
+    private double[] ganacia;
+
     public Estado() {
 
     }
@@ -26,11 +28,13 @@ public class Estado {
 
         this.estado=new int[nClient];
         this.productionNoConsumed=new double[this.centrales.size()];
-        int i=0;
-        for(Central c: this.centrales){
-            this.productionNoConsumed[i]=c.getProduccion();
-            i++;
+
+        for(int i=0; i<this.centrales.size();i++){
+            this.productionNoConsumed[i]=this.centrales.get(i).getProduccion();
         }
+        this.ganacia= new double[this.centrales.size()];
+        for(int i=0; i<this.ganacia.length;i++) this.ganacia[i]=0.0;
+
     }
 
     public void solucionIni() throws myExceptions {
@@ -43,12 +47,43 @@ public class Estado {
             if(cl.getConsumo() <= this.productionNoConsumed[indexCentral]){
                 this.productionNoConsumed[indexCentral]-=cl.getConsumo();
                 this.estado[indexCliente]=indexCentral;
+
+                double p = c.getProduccion();
+                switch (c.getTipo()){
+                    case 0:
+                        this.ganacia[indexCentral]-= p*50+20000;
+                        break;
+                    case 1:
+                        this.ganacia[indexCentral]-= p*80+10000;
+                        break;
+                    default:
+                        this.ganacia[indexCentral]-= p*150+5000;
+                        break;
+                }
             }else{
                 indexCentral++;
                 if(indexCentral<this.centrales.size()) c = this.centrales.get(indexCentral);
                 else throw new myExceptions("No hay suficientes centrales");
             }
             indexCliente++;
+        }
+
+        while(indexCentral<this.centrales.size()){
+            if(this.ganacia[indexCentral]<0){
+                indexCentral++;
+            }else{
+                switch (this.centrales.get(indexCentral).getTipo()){
+                    case 0:
+                        this.ganacia[indexCentral]-= 15000;
+                        break;
+                    case 1:
+                        this.ganacia[indexCentral]-= 5000;
+                        break;
+                    default:
+                        this.ganacia[indexCentral]-= 1500;
+                        break;
+                }
+            }
         }
 
     }
@@ -62,6 +97,10 @@ public class Estado {
         str+="Centrales :";
         for(int i=0; i<this.productionNoConsumed.length; i++){
             str+=" c"+i+"->"+this.productionNoConsumed[i];
+        }
+        str+="\n";
+        for(int i=0; i<this.ganacia.length; i++){
+            str+=" c"+i+"->"+this.ganacia[i];
         }
         return  str;
     }
